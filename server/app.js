411146aabe78ba1cc5cf7ebd3a7e81ece8d86161
Himesh <himesh@gmail.com>
2024-11-20 20:07:23 +0530

    Contact route

--- server/app.js
+++ server/app.js
@@ -5,7 +5,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+
+
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contact', require('./routes/contact'));
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
