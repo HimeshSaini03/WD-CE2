b7c5b80ebe4a8fdb8fce3644009a033d28875a48
KRISHAN KANT <krishankant1705@gmail.com>
2024-11-22 11:49:34 +0530

    routes

--- server/routes/book.js
+++ server/routes/book.js
@@ -29,3 +29,45 @@ router.get("/:token", async (req, res) => {
         res.status(400).send({error: 'Token is invalid'});
     }
 });
+
+router.get("/all/:id", async (req, res) => {
+
+    const {id} = req.params;
+
+    const user = await User.findById(id);
+
+    if(!user)
+    {
+        return res.status(404).send({error: 'User not found'});
+    }
+    
+    const bookings = await Booking.find({userId: id});
+
+    res.status(200).send(bookings);
+});
+
+router.post("/:id", async (req, res) => {
+
+    const {id} = req.params;
+
+    const user = await User.findById(id);
+
+    if(!user)
+    {
+        return res.status(404).send({error: 'User not found'});
+    }
+
+    const Book = await Booking.create({...req.body, userId: id});
+    
+    const bookings = [...user.bookings];
+
+    bookings.push(Book._id);
+
+    user.bookings = bookings;
+
+    await user.save();
+
+    res.status(200).send({message: 'Booking added successfully'});
+});
+
+module.exports = router;
